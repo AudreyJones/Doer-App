542dee7b7168dac1d94cb0f3b98fe6304e646f8f
AudreyJones <audrey.eliz.jones@gmail.com>
2019-10-24 18:50:14 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
 
 import {Image, Navbar} from 'react-bootstrap';
 import Laborocity from './Laborocity.png';
 import AppContainer from './containers/AppContainer';
 
+
+// importing our various Containers and Presentational components...
+import SkillsForm from './components/SkillsForm';
+import Equipment from './components/Equipment';
+import Certifications from './components/Certifications';
+
+
+
+
+
 class App extends Component {
 
   componentDidMount(){
-    fetch("http://localhost:3001/skills")
+    fetch("http://localhost:3000/skills")
     .then(res => res.json())
     .then(skills => this.setState({skills}))
   }
   
-render() {
+  render() {
   console.log("App state:", this.state)
-  return (
-    <Router>
-      <Navbar bg="light">
-          <Navbar.Brand href="#home">
-            <Image src={Laborocity} className="d-inline-block align-top" fluid />
-          </Navbar.Brand>
-      </Navbar>
-
-      <AppContainer />
-
-    </Router>
-  )
-}
+    return (
+   
+      <Router>
+      <div>
+        <Link to="/skills"><button>Skills Form</button></Link>
+        <Link to="/equipment"><button>Equipment</button></Link>
+        <Link to="/certifications"><button>Certifications</button></Link>
+        
+        <Switch>  
+          {/* <Route exact path="/materials/:id" component={MaterialShow}/>
+          <Route exact path="/projects/:id" component={ProjectShow}/> */}
+          <Route exact path="/equipment" component = {Equipment}/>
+          <Route exact path="/skills" component = {SkillsForm}/>
+        </Switch>
+      
+      </div> 
+      </Router>
+    )
+  }
 }
 
 export default App;
